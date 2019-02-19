534d5fee54a81625eaee13c4e80fb95919449f43
yuchen dong <yucdong@microsoft.com>
2019-02-19 17:51:05 +0800

    Implement schedule.go, for testing

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,6 +2,19 @@ package mapreduce
 
 import "fmt"
 
+func worker(workerAddr string, jobs <-chan *DoTaskArgs, results chan<- int) {
+	// worker will accept jobs
+	// and then process them one by one
+	for j := range jobs {
+		ok := call(workerAddr, "Worker.DoTask", j, new(struct{}))
+		if ok == false {
+			fmt.Printf("Run Task failed on %s\n", workerAddr)
+		} else {
+			results <- 1
+		}
+	}
+}
+
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
 // or reducePhase). the mapFiles argument holds the names of the files that
@@ -30,5 +43,32 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	jobs := make(chan *DoTaskArgs, 100)
+	results := make(chan int, 100)
+
+	go func(rc chan string) {
+		for {
+			wk := <-rc
+			go worker(wk, jobs, results)
+		}
+	}(registerChan)
+
+	for i := 0; i < ntasks; i++ {
+		args := new(DoTaskArgs)
+		args.File = mapFiles[i]
+		args.JobName = jobName
+		args.NumOtherPhase = n_other
+		args.Phase = phase
+		args.TaskNumber = i
+
+		jobs <- args
+	}
+	close(jobs)
+
+	for i := 0; i < ntasks; i++ {
+		<-results
+	}
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
